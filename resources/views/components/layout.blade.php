ccb05620ea9228a4a7a5cda797a5a73589cf52d9
Jim Diroff II <jim@dirofftech.com>
2026-01-21 01:51:12 -0500

    feat: new routes and components
    Configured new routes for home page, contact and about us pages. Built new components for test card and general layout

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,37 @@
+@props([
+    'title' => 'Laracasts'
+])
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ $title }}</title>
+
+    <style>
+        nav a {
+            color: blue;
+        }
+        .max-w-400 {
+            max-width: 400px;
+            margin: auto;
+        }
+        .card {
+            background: #e3e3e3;
+            padding: 1rem;
+            text-align: center;"
+        }
+    </style>
+</head>
+<body>
+<nav>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/contact">Contact</a>
+</nav>
+<main>
+    {{ $slot }}
+</main>
+</body>
+</html>
